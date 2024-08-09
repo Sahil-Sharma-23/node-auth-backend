3adc0a56aa95c5f654efbd7b1c4f42868ce5e1cd
Sahil-Sharma-23 <8928515874.ss@gmail.com>
2024-08-09 11:31:06 +0530

    update with new schema

--- schema.sql
+++ schema.sql
@@ -1,9 +1,17 @@
-CREATE DATABASE users;
+CREATE DATABASE auth;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
+    id SERIAL PRIMARY KEY, -- Auto-incrementing ID column
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the record is created
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the record is last updated
+    first_name VARCHAR(255) NOT NULL, -- First name of the user
+    last_name VARCHAR(255) NOT NULL, -- Last name of the user
+    username VARCHAR(255) NOT NULL UNIQUE, -- Username of the user (must be unique)
+    email VARCHAR(255) NOT NULL UNIQUE, -- Email of the user (must be unique)
+    verified BOOLEAN DEFAULT FALSE, -- Whether the user's email is verified
+    password_hash TEXT NOT NULL, -- Hashed password
+    password_salt TEXT NOT NULL, -- Password salt
+    phone_number VARCHAR(20), -- Optional phone number
+    profile_picture TEXT, -- URL to the profile picture
+    gender VARCHAR(50) -- Gender of the user
 );
